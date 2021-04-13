0e7a43364c36aed88403f72b938c74af7d8be999
Jachu05 <jachu05@gmail.com>
2021-04-13 20:26:04 +0200

    try3

--- djangocraigslist/settings.py
+++ djangocraigslist/settings.py
@@ -128,9 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static',
+                    BASE_DIR / 'staticfiles',]
 
 
 django_heroku.settings(locals())
