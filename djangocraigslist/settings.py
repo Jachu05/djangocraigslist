f159ffde6a27634df519b6839ebb6c1f6c05b8eb
Jachu05 <jachu05@gmail.com>
2021-04-13 20:15:55 +0200

    whitenoie added

--- djangocraigslist/settings.py
+++ djangocraigslist/settings.py
@@ -52,6 +52,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'djangocraigslist.urls'
 
 TEMPLATES = [
